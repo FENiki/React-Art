747d6718d23d4e8cbc254c3806e8d4cec65038c9
Fereshte <nikkhah12175@gmail.com>
2024-06-30 11:49:38 +0330

    fixing some bugs

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,7 +3,7 @@ export default function Input({ label, invalid, ...props }) {
   let inputClasses = "w-full px-3 py-2 leading-tight border rounded shadow";
 
   if (invalid) {
-    labelClasses += " text-red-400"; // space is very important to add a new class
+    labelClasses += " text-red-400"; // space is very important to add a new class before the classname
     inputClasses += " text-red-500 bg-red-100 border-red-100";
   } else {
     labelClasses += " text-stone-300";
